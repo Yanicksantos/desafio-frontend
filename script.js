7fb7a7d1ced005c0cc6c263a476de56195a4a1ad
Yanick Santos <yanickeduardo04@outlook.com>
2022-03-27 12:51:43 -0300

    Resolvido a responsividade

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function displayon(){
     document.querySelector(".top-display").style.display="block";
+    document.querySelector("#title").style.fontSize="38px";
 }
 function displayoff(){
     document.querySelector(".top-display").style.display="none";
+    document.querySelector("#title").style.fontSize="49px";
 }
