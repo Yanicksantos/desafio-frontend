c02c407b236546096c63fbcad930d6d85e876d65
Yanick Santos <yanickeduardo04@outlook.com>
2022-03-28 14:45:40 -0300

    Renomeado as classes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function displayon(){
-    document.querySelector(".top-display").style.display="block";
+    document.querySelector(".searchResult").style.display="block";
     document.querySelector("h1").style.fontSize="30px";
 }
 function displayoff(){
-    document.querySelector(".top-display").style.display="none";
+    document.querySelector(".searchResult").style.display="none";
     document.querySelector("h1").style.fontSize="49px";
 }
