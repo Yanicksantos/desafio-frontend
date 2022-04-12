06565626ab9453cce0995840c3eecc757d9982b7
Yanick Santos <yanickeduardo04@outlook.com>
2022-04-12 19:56:11 -0300

    adicionado um alert com conteudo do input (podendo retornar vazio

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 function displayon(){
     document.querySelector(".searchResult").style.display="block";
     document.querySelector("h1").style.fontSize="30px";
+    alert(document.querySelector("#mostraralert").value)
 }
 function displayoff(){
     document.querySelector(".searchResult").style.display="none";
